17b3044b6f26aaf025be69f5b7051aa74f338b19
James Tomasino <james@tomasino.org>
2015-01-03 21:11:36 -0500

    renamed Namespace to NS internally to save 500bytes

--- js/lib/NS.js
+++ js/lib/NS.js
@@ -1,11 +1,11 @@
 (function() {
 
 	// Enable access to global space even in ECMAScript 5 Strict
-	Namespace.global = (function(){ return this || (1,eval)('this') })();
+	NS.global = (function(){ return this || (1,eval)('this') })();
 
-	function Namespace (namespaceString) {
-		var parts = namespaceString.split('.');
-		var parent = Namespace.global;
+	function NS (NSString) {
+		var parts = NSString.split('.');
+		var parent = NS.global;
 		var currentPart = '';
 
 		for(var i = 0, length = parts.length; i < length; i++) {
@@ -17,19 +17,19 @@
 		return parent;
 	}
 
-	Namespace.baseURL = '';
+	NS.baseURL = '';
 
-	Namespace.import = function ( namespaceString ) {
-		var parts = namespaceString.split('.'),
-		parent = Namespace.global,
+	NS.import = function ( NSString ) {
+		var parts = NSString.split('.'),
+		parent = NS.global,
 		currentPart = '';
 
 		for ( var i = 0, length = parts.length; i < length; i++ ) {
 			currentPart = parts[i];
 			if ( typeof parent[currentPart] == 'undefined') {
-				Namespace.load ( namespaceString );
+				NS.load ( NSString );
 				if ( typeof parent[currentPart] == 'undefined') {
-					throw ('ERROR::[ Namespace does not exist: ' + namespaceString + ' ]' );
+					throw ('ERROR::[ Namespace does not exist: ' + NSString + ' ]' );
 					return;
 				}
 			}
@@ -39,34 +39,34 @@
 		return parent;
 	}
 
-	Namespace.load = function ( namespaceString ) {
-		var xhrObj = Namespace.createXMLHTTPObject();
-		var scriptURL = namespaceString;
+	NS.load = function ( NSString ) {
+		var xhrObj = NS.createXMLHTTPObject();
+		var scriptURL = NSString;
 		while (scriptURL.indexOf('.') != -1) {
 			scriptURL = scriptURL.replace('.', '/');
 		}
 		scriptURL = scriptURL + '.js';
-		xhrObj.open('GET', Namespace.baseURL + scriptURL, false);
+		xhrObj.open('GET', NS.baseURL + scriptURL, false);
 		xhrObj.send('');
-		var se = Namespace.global.document.createElement('script');
+		var se = NS.global.document.createElement('script');
 		se.type = "text/javascript";
 		se.text = xhrObj.responseText;
-		Namespace.global.document.getElementsByTagName('head')[0].appendChild(se);
+		NS.global.document.getElementsByTagName('head')[0].appendChild(se);
 	}
 
-	Namespace.XMLHttpFactories = [
+	NS.XMLHttpFactories = [
 		function () {return new XMLHttpRequest()},
 		function () {return new ActiveXObject("Msxml2.XMLHTTP")},
 		function () {return new ActiveXObject("Msxml3.XMLHTTP")},
 		function () {return new ActiveXObject("Microsoft.XMLHTTP")}
 	];
 
-	Namespace.createXMLHTTPObject = function () {
+	NS.createXMLHTTPObject = function () {
 		var xmlhttp = false;
-		for (var i = 0; i < Namespace.XMLHttpFactories.length; i++) {
+		for (var i = 0; i < NS.XMLHttpFactories.length; i++) {
 
 			try {
-				xmlhttp = Namespace.XMLHttpFactories[i]();
+				xmlhttp = NS.XMLHttpFactories[i]();
 			} catch (e) {
 				continue;
 			}
@@ -75,7 +75,7 @@
 		return xmlhttp;
 	}
 
-	Namespace.global.NS = Namespace;
+	NS.global.NS = NS;
 
 })();
 
