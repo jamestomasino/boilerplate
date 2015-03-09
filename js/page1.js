27f65b4d5ea46a613c1b85658eb7728970321939
James Tomasino <james@tomasino.org>
2015-03-09 15:04:21 -0400

    added examples to page1.js

--- js/page1.js
+++ js/page1.js
@@ -13,4 +13,27 @@ model.applicationStart();
 // bidirectional data binding example
 var SampleBind = NS.import('lib.Bind');
 var testbind = new SampleBind('testbind');
-testbind.set('testbindprop', 600);
+testbind.set('testbindprop', 600); // Default
+
+// DOM example
+var DOM = NS.import('lib.DOM');
+var wrapperEl = DOM.get('.wrapper')[0]; // Get first element of class array
+var DOMtestEl = DOM.create('<p>This is a dynamically created p tag.</p>');
+wrapperEl.appendChild(DOMtestEl);
+
+// LocalStorage Example
+
+// Check for stored testbindprop value and use it if found
+var Storage = NS.import('lib.Storage');
+var storageTestbindprop = Storage.get('testbindprop');
+if (storageTestbindprop) testbind.set('testbindprop', storageTestbindprop);
+
+// Listen for bound data object and store testbindprop if changed
+var Events = NS.import('lib.Events');
+Events.subscribe( testbind.updateMessage, function ( propName, val ){
+	if (propName === 'testbindprop') {
+		Storage.set ('testbindprop', val);
+	}
+});
+
+
