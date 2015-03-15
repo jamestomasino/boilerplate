2195d52d2488d390d0288f3308d51e77b265790a
James Tomasino <james@tomasino.org>
2015-03-15 16:34:45 -0400

    formatting

--- js/lib/NS.js
+++ js/lib/NS.js
@@ -3,13 +3,11 @@
 		if (!window.console) {
 			window.console = {};
 		}
-		// union of Chrome, FF, IE, and Safari console methods
 		var m = [
 			"log", "info", "warn", "error", "debug", "trace", "dir", "group",
 			"groupCollapsed", "groupEnd", "time", "timeEnd", "profile", "profileEnd",
 			"dirxml", "assert", "count", "markTimeline", "timeStamp", "clear"
 		];
-		// define undefined methods as noops to prevent errors
 		for (var i = 0; i < m.length; i++) {
 			if (!window.console[m[i]]) {
 				window.console[m[i]] = function() {};
@@ -224,4 +222,3 @@
 	NS.global.NS = NS;
 
 })();
-
