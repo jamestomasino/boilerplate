f3ac2536b1470fe2df56e8c5aabcdeb2681a28b8
James Tomasino <james@tomasino.org>
2015-03-14 22:35:45 -0400

    added docs to page setup

--- js/page1.js
+++ js/page1.js
@@ -1,44 +1,61 @@
-
+// Set the global path to javascript files for NS
 NS.baseURL = 'js/';
 
-var libs = ['app.model.SampleModel', 'app.controller.SampleController', 'app.view.SampleView', 'lib.Bind', 'lib.DOM', 'lib.Storage', 'lib.Events'];
+// List all page dependencies
+var libs = ['app.model.SampleModel', 'app.controller.SampleController',
+	'app.view.SampleView', 'lib.Bind', 'lib.DOM', 'lib.Storage', 'lib.Events'];
 
+// Load all page dependencies and initiate page setup via callback
 NS.load ( libs, page1, this);
 
+// Page level callback
 function page1 () {
 
-	var SampleModel = NS.use('app.model.SampleModel');
-	var SampleController = NS.use('app.controller.SampleController');
-	var SampleView = NS.use('app.view.SampleView');
+	/**********************************************************************/
+	/**************************** MVC Example *****************************/
+	/**********************************************************************/
+	var SampleModel         = NS.use('app.model.SampleModel');
+	var SampleController    = NS.use('app.controller.SampleController');
+	var SampleView          = NS.use('app.view.SampleView');
 
-	var model = new SampleModel();
-	var controller = new SampleController( model );
-	var view = new SampleView( '#sample_id' );
+	var model               = new SampleModel();
+	var controller          = new SampleController( model );
+	var view                = new SampleView( '#sample_id' );
 
 	model.applicationStart();
 
-	// bidirectional data binding example
-	var Bind = NS.use('lib.Bind');
-	var testbind = new Bind('testbind');
-	testbind.set('testbindprop', 600); // Default
+	/**********************************************************************/
+	/********************* Bidirectional Data Binding *********************/
+	/**********************************************************************/
+	var Bind                = NS.use('lib.Bind');
+	var testbind            = new Bind('testbind');
 
-	// DOM example
-	var DOM = NS.use('lib.DOM');
-	var wrapperEls = DOM.find('.wrapper');
-	var mainWrapper = wrapperEls[0];
-	var DOMtestEl = DOM.create('<p>This is a dynamically created p tag.</p>');
+	testbind.set('testbindprop', 600);
+
+	/**********************************************************************/
+	/***************************** DOM Example ****************************/
+	/**********************************************************************/
+	var DOM                 = NS.use('lib.DOM');
+	var wrapperEls          = DOM.find('.wrapper');
+	var mainWrapper         = wrapperEls[0];
+	var DOMtestEl           = DOM.create('<p>Dynamically created tag.</p>');
+
 	mainWrapper.appendChild(DOMtestEl);
 
-	// LocalStorage Example
+	/**********************************************************************/
+	/************************ LocalStorage Example ************************/
+	/**********************************************************************/
 
 	// Check for stored testbindprop value and use it if found
-	var Storage = NS.use('lib.Storage');
+	var Storage             = NS.use('lib.Storage');
 	var storageTestbindprop = Storage.get('testbindprop');
+
 	if (storageTestbindprop) testbind.set('testbindprop', storageTestbindprop);
 
 	// Listen for bound data object and store testbindprop if changed
-	var Events = NS.use('lib.Events');
-	Events.subscribe( testbind.updateMessage, function ( propName, val ){
+	var Events              = NS.use('lib.Events');
+
+	Events.subscribe( testbind.updateMessage, function ( propName, val ) {
 		if (propName === 'testbindprop') {
 			Storage.set ('testbindprop', val);
 		}
