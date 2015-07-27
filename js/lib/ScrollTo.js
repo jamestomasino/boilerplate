8c2f9a34d25dc9b8ac1075ca90f408df2feafab7
James Tomasino <james@tomasino.org>
2015-07-27 13:54:33 -0400

    updating multiple libs and adding polyfills

---
+++ js/lib/ScrollTo.js
@@ -0,0 +1,59 @@
+(function (NS) {
+	"use strict"
+
+	var supportPageOffset = window.pageXOffset !== undefined;
+	var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
+
+	function classWrapper() {
+
+		function ScrollTo(to, callback, duration, easing) {
+
+			function move(amount) {
+				document.documentElement.scrollTop = document.body.parentNode.scrollTop = document.body.scrollTop = amount;
+			}
+
+			function position() {
+				return document.documentElement.scrollTop || document.body.parentNode.scrollTop || document.body.scrollTop;
+			}
+
+			if (typeof(to) === 'string') {
+				to = parseInt(to,10) + position();
+			}
+
+			var start = position();
+			var change = to - start;
+			var currentTime = 0;
+			var increment = 20;
+
+			duration = (typeof(duration) === 'undefined') ? 500 : duration;
+			easing = (typeof(easing) === 'undefined') ? Math.easeInOutQuad : easing;
+
+			var animateScroll = function() {
+				currentTime += increment;
+				var val = easing.call(this, currentTime, start, change, duration);
+				move(val);
+				if (currentTime < duration) {
+					requestAnimationFrame(animateScroll);
+				} else {
+					if (callback && typeof(callback) === 'function') {
+						callback();
+					}
+				}
+			};
+
+			animateScroll();
+		}
+
+		var namespace = new NS ( 'lib' );
+		namespace.ScrollTo = ScrollTo;
+	}
+
+	var polyfills = [];
+
+	if (!window.requestAnimationFrame) {
+		polyfills.push("polyfill.requestAnimationFrame");
+	}
+
+	NS.load ( ['lib.Easing'].concat(polyfills), classWrapper, this );
+
+})(window.NS);
